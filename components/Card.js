f21390019b715395da25d15010ca6aaa4ce0dbe6
Mustapha HAMADI <hamadi.mustapha7@gmail.com>
2022-04-22 14:50:02 +0200

    create card component

--- components/Card.js
+++ components/Card.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import CountUp from 'react-countup'
 
-const Card = () => {
-  return <div>Card</div>
+const Card = ({ title, amount, color }) => {
+  return (
+    <div
+      className={`w-1/3 rounded-lg border border-${color}-200 bg-${color}-200 p-6 shadow-md hover:bg-${color}-100 `}
+    >
+      <h5 className="mb-5 text-2xl font-bold tracking-tight text-gray-900 ">
+        Revenue
+      </h5>
+      <p className="text-3xl font-bold text-gray-700">
+        <CountUp end={100} decimals={2} suffix=" €" duration={1} />
+      </p>
+    </div>
+  )
 }
 
 export default Card
--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <div className="h-24 w-full bg-gray-200 p-9">
+      <h1 className="ml-4 text-2xl font-bold uppercase tracking-wider">
+        Memory
+      </h1>
+    </div>
+  )
 }
 
 export default Header
